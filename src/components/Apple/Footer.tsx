0b1022831f01a0900e68f8a6784547751228823a
ure <uy23@naver.com>
2023-10-02 00:06:20 +0900

    feat #1: footer css

--- src/components/Apple/Footer.tsx
+++ src/components/Apple/Footer.tsx
@@ -1,8 +1,12 @@
 import styled from "@emotion/styled";
 
-const Container = styled.footer``;
+const Container = styled.footer`
+  margin-top: auto;
+  background: #f5f5f7;
+  color: #6e6e73;
+`;
 function Footer() {
-  return <Container>Footer</Container>;
+  return <Container>Footer입니다.</Container>;
 }
 
 export default Footer;
